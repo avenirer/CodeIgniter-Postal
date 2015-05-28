cfae2a4d6cbf166c16a731c67b944aee440eb2df
Adrian Voicu <avenir.ro@gmail.com>
2015-05-28 11:48:12 +0300

    first commit of code
    first commig

---
+++ config/postal.php
@@ -0,0 +1,5 @@
+<?php
+$config['message_types'] = array(
+    'success' => array('<div class="alert alert-info alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>', '</div>'),
+    'error' => array('<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>', '</div>'),
+    'message' => array('<div class="message">', '</div>'));
